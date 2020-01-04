32dede341dcf9e3910d9736ecb4809b91d94a445
William Mendes <will.smendes@gmail.com>
2020-01-04 17:59:42 +0000

    [fix] removing celery

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from celery import Celery
 from flask_restful import Api
 from resources.application import Application, ApplicationList
 import database
@@ -7,35 +6,13 @@ import database
 app = Flask(__name__)
 api = Api(app)
 
-app.config.from_object('config')
 
 api.add_resource(ApplicationList, '/applications')
 api.add_resource(Application, '/application/<app_id>','/application')
 
 database.initialize_db()
 
-
-def make_celery(app):
-    # create context tasks in celery
-    celery = Celery(
-        app.import_name,
-        broker=app.config['BROKER_URL']
-    )
-    TaskBase = celery.Task
-
-    class ContextTask(TaskBase):
-        abstract = True
-
-        def __call__(self, *args, **kwargs):
-            with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
-
-    celery.Task = ContextTask
-
-    return celery
-
-celery = make_celery(app)
-
+ 
 
 if __name__ == '__main__':
     app.run(debug=True)
